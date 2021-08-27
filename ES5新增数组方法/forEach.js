ad0a34b56fa17bee4db5f01045036585ac4a75c6
yepi-bit <1607562910@qq.com>
2021-08-27 21:39:39 +0800

    css样式

---
+++ ES5新增数组方法/forEach.js
@@ -0,0 +1,8 @@
+
+var arr = [1,2,3,4,5,6];
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i], i, arr);
+// }
+arr.forEach(function (e,i,array) {
+    console.log(e, i, array);
+})
